beb147d3f50096a5e93ff37c7d56db17cd0a9621
yuzuru.takemura <y.takemura@mis.co.jp>
2022-02-17 07:57:42 +0000

    resubmit migration file

--- database/migrations/2022_02_17_162002_add_status_to_tasks_table.php
+++ database/migrations/2022_02_17_162002_add_status_to_tasks_table.php
@@ -26,7 +26,7 @@ class AddTitleToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table -> string('status', 10); 
+            $table -> dropColumn('status'); 
         });
     }
 }
